b565c80572d306af8caf7bfd7903599ff4062180
zicaacademy <138627459+zicaacademy@users.noreply.github.com>
2024-09-17 16:25:19 -0300

    create project

---
+++ MAcro02-Grupo02/MAcro02-Grupo02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAcro02-Grupo02
+//
+//  Created by Luiz Felipe on 17/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MAcro02-Grupo02/MAcro02-Grupo02Tests/MAcro02_Grupo02Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MAcro02_Grupo02Tests.swift
+//  MAcro02-Grupo02Tests
+//
+//  Created by Luiz Felipe on 17/09/24.
+//
+
+import Testing
+@testable import MAcro02_Grupo02
+
+struct MAcro02_Grupo02Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
