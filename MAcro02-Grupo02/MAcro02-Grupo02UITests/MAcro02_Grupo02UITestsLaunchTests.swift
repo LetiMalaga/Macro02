b6316b3bce0b03b11ff57b0c3a12dade7bf1d48b
zicaacademy <138627459+zicaacademy@users.noreply.github.com>
2024-09-25 17:48:51 -0300

    bug - remocao dos testes

--- MAcro02-Grupo02/MAcro02-Grupo02UITests/MAcro02_Grupo02UITestsLaunchTests.swift
+++ MAcro02-Grupo02/MAcro02-Grupo02UITests/MAcro02_Grupo02UITestsLaunchTests.swift
@@ -1,33 +1,33 @@
+////
+////  MAcro02_Grupo02UITestsLaunchTests.swift
+////  MAcro02-Grupo02UITests
+////
+////  Created by Luiz Felipe on 17/09/24.
+////
 //
-//  MAcro02_Grupo02UITestsLaunchTests.swift
-//  MAcro02-Grupo02UITests
+//import XCTest
 //
-//  Created by Luiz Felipe on 17/09/24.
-//
-
-import XCTest
-
-final class MAcro02_Grupo02UITestsLaunchTests: XCTestCase {
-//
-//    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-//        true
-//    }
-//
-//    override func setUpWithError() throws {
-//        continueAfterFailure = false
-//    }
-//
-//    @MainActor
-//    func testLaunch() throws {
-//        let app = XCUIApplication()
-//        app.launch()
-//
-//        // Insert steps here to perform after app launch but before taking a screenshot,
-//        // such as logging into a test account or navigating somewhere in the app
-//
-//        let attachment = XCTAttachment(screenshot: app.screenshot())
-//        attachment.name = "Launch Screen"
-//        attachment.lifetime = .keepAlways
-//        add(attachment)
-//    }
-}
+//final class MAcro02_Grupo02UITestsLaunchTests: XCTestCase {
+////
+////    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+////        true
+////    }
+////
+////    override func setUpWithError() throws {
+////        continueAfterFailure = false
+////    }
+////
+////    @MainActor
+////    func testLaunch() throws {
+////        let app = XCUIApplication()
+////        app.launch()
+////
+////        // Insert steps here to perform after app launch but before taking a screenshot,
+////        // such as logging into a test account or navigating somewhere in the app
+////
+////        let attachment = XCTAttachment(screenshot: app.screenshot())
+////        attachment.name = "Launch Screen"
+////        attachment.lifetime = .keepAlways
+////        add(attachment)
+////    }
+//}
