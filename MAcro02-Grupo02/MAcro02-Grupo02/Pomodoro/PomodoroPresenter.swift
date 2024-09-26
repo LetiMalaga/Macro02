220b984a0b602242ecb344128c8f66183d6932e5
Luca <paesberaldiluca@gmail.com>
2024-09-26 17:20:24 -0300

    US09 fix: Pomodoro loop wasn't working properly

--- MAcro02-Grupo02/MAcro02-Grupo02/Pomodoro/PomodoroPresenter.swift
+++ MAcro02-Grupo02/MAcro02-Grupo02/Pomodoro/PomodoroPresenter.swift
@@ -11,22 +11,28 @@ import Foundation
 protocol PomodoroPresenterProtocol {
     func displayTime(_ time: String)
     func resetPomodoro()
-    func updateButton(isRunning: Bool, isPaused: Bool)  // Add this
+    func updateButton(isRunning: Bool, isPaused: Bool)
+    func updateStateLabel(_ text: String)  // New function to update the state label
 }
 
 class PomodoroPresenter: PomodoroPresenterProtocol {
     weak var viewController: PomodoroViewController?
-    
+
     func displayTime(_ time: String) {
         viewController?.displayTime(time)
     }
-    
+
     func resetPomodoro() {
         viewController?.displayTime("25:00")
         viewController?.updateButton(isRunning: false, isPaused: false)
+        viewController?.updateStateLabel("Time to Work!")
     }
 
     func updateButton(isRunning: Bool, isPaused: Bool) {
-        viewController?.updateButton(isRunning: isRunning, isPaused: isPaused)  // Relay button update to view
+        viewController?.updateButton(isRunning: isRunning, isPaused: isPaused)
+    }
+
+    func updateStateLabel(_ text: String) {
+        viewController?.updateStateLabel(text)
     }
 }
