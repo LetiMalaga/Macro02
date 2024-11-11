b862f586fa0a32eb3b8485baf69c143843524488
LetiMalaga <leticiamalagutti@yahoo.com.br>
2024-11-11 13:50:51 -0300

    Ajustes de proporção iniciais

---
+++ MAcro02-Grupo02/MAcro02-Grupo02/Helpers/ColorExtensions.swift
@@ -0,0 +1,13 @@
+//
+//  ColorExtensions.swift
+//  MAcro02-Grupo02
+//
+//  Created by Letícia Malagutti on 07/11/24.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let systemOpposingColor = UIColor(named: "SystemOpposingColor")
+}
